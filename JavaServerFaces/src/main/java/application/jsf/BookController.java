5abe9193f76396e08c9ee479621a6502244275f5
N Mhlongo <you@example.com>
2017-07-27 17:25:03 +0200

    pushed jsf

--- JavaServerFaces/src/main/java/application/jsf/BookController.java
+++ JavaServerFaces/src/main/java/application/jsf/BookController.java
@@ -4,5 +4,14 @@ import javax.faces.bean.ManagedBean;
 
 @ManagedBean ( name = "bookController" )
 public class BookController {
-  
+
+  private String isbn;
+
+  public String getIsbn() {
+    return isbn;
+  }
+
+  public void setIsbn( String isbn ) {
+    this.isbn = isbn;
+  }
 }
