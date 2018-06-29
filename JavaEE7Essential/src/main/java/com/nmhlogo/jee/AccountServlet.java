d7ef94bbe503bc8c18fd8fb4dbd105c966a825b5
Ndumiso <ndumisosizwe@gmail.com>
2018-06-29 19:42:30 +0200

    start JavaEE7Essentials

---
+++ JavaEE7Essential/src/main/java/com/nmhlogo/jee/AccountServlet.java
@@ -0,0 +1,42 @@
+package com.nmhlogo.jee;
+
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebInitParam;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@WebServlet(urlPatterns = { "/account/*", "/" }, initParams = { @WebInitParam(name = "type", value = "checkAccount") })
+public class AccountServlet extends HttpServlet {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+
+	@Override
+	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		req.getRequestDispatcher("views/home.jsp").forward(req, resp);
+	}
+
+	@Override
+	protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		super.doPost(req, resp);
+	}
+
+	@Override
+	protected void doPut(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		super.doPut(req, resp);
+	}
+
+	@Override
+	public void init() throws ServletException {
+		System.out.println("Servlet started...");
+	}
+
+}
---
+++ JavaEE7Essential/src/main/webapp/WEB-INF/views/home.jsp
@@ -0,0 +1,6 @@
+<title>MyServlet</title>
+</head>
+<body>
+	<h1>My First Servlet</h1>
+</body>
+</html>
